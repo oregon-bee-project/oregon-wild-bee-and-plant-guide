c416d66b11d22aa1bf0a2eaf9c5e2b8f1e60bf60
alexmontgomery <alexrm.1212@gmail.com>
2025-11-03 15:58:07 -0800

    Add customization to base App

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import beeImage from './assets/bee.jpg'
+import plantImage from './assets/plant.jpg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={beeImage} class="logo" />
+        <img src={plantImage} class="logo" />
+
       </div>
-      <h1>Vite + React</h1>
+      <h1>Bee and Plant Capstone Project</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click if you love bees!
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>❤️ {count}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
