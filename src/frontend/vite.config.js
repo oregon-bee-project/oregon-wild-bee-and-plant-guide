a76c21d680d57b8393748e7da9128cd8192ffb85
henrykanaskie <howenkan13@gmail.com>
2025-11-07 12:20:01 -0800

    fix vite config

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/bee-plant-data-exploration/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
     globals: true,
   },
 });
