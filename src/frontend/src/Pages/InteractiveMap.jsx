148fe106023185d5dd5833aa301d54996c92babb
alexmontgomery <alexrm.1212@gmail.com>
2025-11-10 15:01:49 -0800

    Change style path for map

--- src/frontend/src/Pages/InteractiveMap.jsx
+++ src/frontend/src/Pages/InteractiveMap.jsx
@@ -13,7 +13,7 @@ const InteractiveMap = () => {
 
     const map = new maplibregl.Map({
       container: mapContainerRef.current,
-      style: "./custom-map-style.json",
+      style: import.meta.env.BASE_URL + "custom-map-style.json",
       center: [-120, 44],
       zoom: 6,
     });
