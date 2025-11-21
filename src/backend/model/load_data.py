92954921bd94b51e1379b9e247fe6d2fb50812f5
henrykanaskie <howenkan13@gmail.com>
2025-11-21 12:07:59 -0800

    feat: add load data function for model

--- src/backend/model/load_data.py
+++ src/backend/model/load_data.py
@@ -5,15 +5,32 @@ Calls parse_viz.py to get a dataframe from the viz
 files. Then cleans the data into a modeling ready state.
 """
 
+import sys
+import os
 import pandas as pd
-from ..parse_viz import parse_viz_to_dataframe
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
+from parse_viz import parse_viz_to_dataframe
+
+"""
+Load the data into two dataframes and returns them.
+"""
+def load_data() -> pd.DataFrame:
+    file_path_labels = "data/b-team/plant-pollinators-OBA-2025-assigned-subset-labels.viz"
+    file_path_taxa = "data/b-team/plant-pollinators-OBA-2025-assigned-taxa.viz"
+    dflabels = parse_viz_to_dataframe(file_path_labels)
+    dftaxa = parse_viz_to_dataframe(file_path_taxa)
+    print("\nLabels DataFrame:\n")
+    print(dflabels.head())
+    print("\nTaxa DataFrame:\n")
+    print(dftaxa.head())
+    return dflabels, dftaxa
 
 def main() -> None:
-    file_path = "../data/b-team/plant-pollinators-OBA-2025-assigned-subset-labels.viz"
-    file_path = "../data/b-team/plant-pollinators-OBA-2025-assigned-taxa.viz"
-    df = parse_viz_to_dataframe(file_path)
-    print(df.head())
+    load_data()
 
 
 if __name__ == "__main__":
