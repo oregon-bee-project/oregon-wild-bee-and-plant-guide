a556892a9fae0de3813b43da676126c3811ac249
henrykanaskie <howenkan13@gmail.com>
2025-11-24 20:26:02 -0800

    move df loading out of request and add stress test

--- src/backend/main.py
+++ src/backend/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
 full_df = pv.parse_viz_to_dataframe("../data/b-team/plant-pollinators-OBA-2025-assigned-subset-labels.viz")
 inat_key = pv.parse_viz_to_dataframe("../data/b-team/plant-pollinators-OBA-2025-assigned-taxa.viz")
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -40,7 +41,7 @@ def location_root(lat: float, long: float):
     
     if response_json["error"]:
         return response_json
-    
+
     sl.filter_df(response_json, full_df)
     
     sl.summary_stats(response_json, inat_key)
