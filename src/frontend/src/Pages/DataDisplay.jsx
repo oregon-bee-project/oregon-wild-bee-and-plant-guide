bc542eea1255544df998ebe22fb08600db322f20
alexmontgomery <alexrm.1212@gmail.com>
2025-12-01 21:52:23 -0800

    Remove skeleton loader

--- src/frontend/src/Pages/DataDisplay.jsx
+++ src/frontend/src/Pages/DataDisplay.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Skeleton, Button } from "@chakra-ui/react";
+import { Box, Flex, Button } from "@chakra-ui/react";
 import { LuFileUp } from "react-icons/lu";
 import BeeStatsPanel from "../CustomComponents/BeeStatsPanel";
 
@@ -15,15 +15,7 @@ const DataDisplay = ({ locationData, selectedCoords }) => {
         p={2}
         overflowY="auto"
       >
-        {locationData && locationData.lat == selectedCoords.lat && locationData.long == selectedCoords.lng ? (
-          <BeeStatsPanel data={locationData} />
-        ) : (
-            <Box>
-                {Array.from({ length: 9 }).map((_, i) => (
-                    <Skeleton key={i} height="30px" width="100%" mb="18px" />
-                ))}
-            </Box>
-        )}
+        {locationData && <BeeStatsPanel data={locationData} />}
       </Box>
       <Flex gap="8px">
         <Button flex="1" bg="blue.600" _hover={{ bg: "blue.500" }}>
